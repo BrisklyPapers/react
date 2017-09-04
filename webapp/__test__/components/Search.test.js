8c18313c14ed8c103fbf58a04ceccf201a8dfd1e
Sebastian Detert <github@elygor.de>
2017-09-04 23:42:15 +0200

    added missing tests

--- webapp/__test__/components/Search.test.js
+++ webapp/__test__/components/Search.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Search from '../../src/components/Search.js';
 import renderer from 'react-test-renderer';
+import {mount} from 'enzyme';
+import {spy} from 'sinon';
 
 describe('components/Search', () => {
     it('renders correctly', () => {
@@ -8,5 +10,38 @@ describe('components/Search', () => {
             <Search searchedText="test" triggerSearch={() => {}}/>
         );
         expect(search.toJSON()).toMatchSnapshot();
+    });
+
+    it('triggers search on change', (done) => {
+        const callbackSpy = spy();
+        const wrapper = mount(
+            <Search searchedText="test" triggerSearch={callbackSpy}/>
+        );
+
+        const event = {key: 'f', target: {value: 'f'}};
+        wrapper.find('TextField').first().find('input').simulate('change', event);
+
+        expect.assertions(2);
+        setTimeout(() => {
+            expect(callbackSpy.callCount).toEqual(1);
+            expect(callbackSpy.calledWithMatch(event)).toEqual(true);
+            done();
+        }, 0);
+    });
+
+    it('triggers callback on focus', (done) => {
+
+        const callbackSpy = spy();
+        const wrapper = mount(
+            <Search searchedText="test" triggerSearch={() => {}} onFocus={callbackSpy}/>
+        );
+
+        wrapper.find('TextField').first().find('input').simulate('focus');
+
+        expect.assertions(1);
+        setTimeout(() => {
+            expect(callbackSpy.callCount).toEqual(1);
+            done();
+        }, 0);
     });
 });
--- webapp/__test__/components/SearchResults.test.js
+++ webapp/__test__/components/SearchResults.test.js
@@ -4,9 +4,29 @@ import renderer from 'react-test-renderer';
 
 describe('components/SearchResults', () => {
     it('renders correctly', () => {
-        const documents = [];
         const searchResults = renderer.create(
             <SearchResults documents={[]}/>
+        );
+        expect(searchResults.toJSON()).toMatchSnapshot();
+    });
+
+    it('contains some search results', () => {
+        const documents = [
+            {
+                fileName: 'bill.pdf',
+                url: 'http://briskly-papers.com',
+                text: ['testing search results'],
+                tags: ['bill', 'source']
+            },
+            {
+                fileName: 'flower.jpg',
+                url: 'https://localhost/search?q=flowers',
+                text: ['pictures showing some flowers'],
+                tags: ['flower', 'summer']
+            }
+        ];
+        const searchResults = renderer.create(
+            <SearchResults documents={documents}/>
         );
         expect(searchResults.toJSON()).toMatchSnapshot();
     });
