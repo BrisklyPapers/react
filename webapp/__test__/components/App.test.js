4e2bd24ff81bcf44fd0d0d2384bdc1e9df9bffd9
Sebastian Detert <github@elygor.de>
2017-09-05 07:58:29 +0200

    removed old code

--- webapp/__test__/components/App.test.js
+++ webapp/__test__/components/App.test.js
@@ -4,14 +4,12 @@ const ReactShallowRenderer = require('react-test-renderer/shallow');
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import fetchMock from 'fetch-mock'
-import configureMockStore from 'redux-mock-store'
 
-const mockStore = configureMockStore();
 const renderer = new ReactShallowRenderer();
 
 describe('components/App', () => {
     it('renders correctly', () => {
-        const app = renderer.render(
+        renderer.render(
             <App />
         );
         expect(renderer.getRenderOutput()).toMatchSnapshot();
